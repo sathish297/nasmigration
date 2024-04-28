f84463d6a3977836375232c87ac9240f1986c8a8
sathish297 <34938948+sathish297@users.noreply.github.com>
2024-04-28 15:04:05 +0100

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,7 +1,7 @@
 
 @echo_off
-for /f "delims=/tokens=1,2" %%a in ('date /t') do set date=%%a_%%b
-for /f "delims=/tokens=1,2,3" %%a in ('time /t') do set time=%%H_%%M
+for /f "delims=/ tokens=1,2" %%a in ('date /t') do set date=%%c-%%b-%%a
+for /f "delims=: tokens=1,2,3" %%a in ('time /t') do set time=%%a:%%b
 
 forfiles /P .\logs /M *.txt /D -7 /C "cmd /c del @file" >nul 2>nul
 
