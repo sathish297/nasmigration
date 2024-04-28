74346da77b317da2f16b9e665911948a1641696d
sathish297 <34938948+sathish297@users.noreply.github.com>
2024-04-28 15:04:34 +0100

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -5,5 +5,5 @@ for /f "delims=: tokens=1,2,3" %%a in ('time /t') do set time=%%a:%%b
 
 forfiles /P .\logs /M *.txt /D -7 /C "cmd /c del @file" >nul 2>nul
 
-robocopy c:\scripts\data\source c:\scripts\data\target /XO * /MIR /R:1 /W:1 /LOG:"c:\scripts\logs\copy_%date%.txt"
+robocopy c:\scripts\data\source c:\scripts\data\target /XO * /MIR /R:1 /W:1 /LOG:"c:\scripts\logs\copy_%date%%time%.txt"
 
